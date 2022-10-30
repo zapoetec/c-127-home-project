665d17ad47216ff3418e56f843f0e6a514889f0e
zapoetec <91003961+zapoetec@users.noreply.github.com>
2022-10-30 12:58:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var music1="";
+var music2="";
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,600,500);
+}
+function preload(){
+music1=loadSound("music.mp3");
+music2=loadSound("music2.mp3");
+}
